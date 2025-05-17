bb8e13ecd4f4ba32a71edd168507c0a615e8c2b5
PixelSejal <158022557+PixelSejal@users.noreply.github.com>
2025-05-17 19:04:06 +0530

    Create Literals.java

---
+++ src/Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args){
+         byte age = 20;
+         char ch = 's';
+         float f1 = 5.6f;
+         double d1 = 4.66;
+         long ageDino = 566666666666L;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(ageDino);
+        System.out.println("what a waste this was :(");
+    }
+}
