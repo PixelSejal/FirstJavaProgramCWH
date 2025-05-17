8bcf86013b2d2782118a6d9c0211addad90970c4
PixelSejal <158022557+PixelSejal@users.noreply.github.com>
2025-05-17 18:50:48 +0530

    Create Variables.java

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args){
+        System.out.print("the sum of the follwoing number is ");
+        int a = 10;
+        int b = 176;
+        int c = 89;
+        int sum = a+b+c;
+        System.out.print(sum);
+    }
+}
