cd775f1ab9e4c8621b774a2165b1a40834c3ef01
PixelSejal <158022557+PixelSejal@users.noreply.github.com>
2025-08-05 23:04:45 +0530

    Create UserInput.java

---
+++ src/UserInput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class UserInput {
+    public static void main(String[] args){
+        System.out.println("taking input from the user : ");
+        Scanner S = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = S.nextInt();
+
+        System.out.print("Enter number 2: ");
+        int num2 = S.nextInt();
+
+        System.out.print("Enter number 3: ");
+        int num3 = S.nextInt();
+
+        // Output the numbers
+        System.out.println("You entered:");
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Number 3: " + num3);
+
+        S.close();
+
+
+    }
+}
