f98c5481a18ac61235189212038451062651a6e2
PixelSejal <158022557+PixelSejal@users.noreply.github.com>
2025-08-06 17:15:22 +0530

    Create AreaOfCircle.java

---
+++ src/AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AreaOfCircle {
+    public static void main (String[] args){
+        System.out.println("enter the radius of circle");
+        Scanner sc = new Scanner(System.in);
+double radius = sc.nextDouble();
+double area = Math.PI*radius*radius;
+        System.out.println(area);
+sc.close ();
+          }
+}
