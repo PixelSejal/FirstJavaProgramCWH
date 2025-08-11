651e75cced4aa01472a8365d56e7943df3326ec4
PixelSejal <158022557+PixelSejal@users.noreply.github.com>
2025-08-11 13:53:55 +0530

    Create MorePatterns.java

---
+++ src/MorePatterns.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+class MorePatterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size (n): ");
+        int n = sc.nextInt();
+
+        System.out.println("\nButterfly Pattern:");
+        printButterfly(n);
+
+        System.out.println("\nDiamond Pattern:");
+        printDiamond(n);
+
+        sc.close();
+    }
+
+    // Butterfly Pattern
+    public static void printButterfly(int n) {
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            // 1st half stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // 2nd half stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = n; i >= 1; i--) {
+            // 1st half stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // 2nd half stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Diamond Pattern
+    public static void printDiamond(int n) {
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = n - 1; i >= 1; i--) { // start from n-1 to avoid middle line twice
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
