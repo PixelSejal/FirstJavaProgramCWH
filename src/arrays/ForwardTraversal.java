e5435481829292ca4d2af08ca8d3156577d8cf5c
PixelSejal <158022557+PixelSejal@users.noreply.github.com>
2025-08-23 12:30:04 +0530

    Create ForwardTraversal.java
    Added ForwardTraversal.java program in arrays package

---
+++ src/arrays/ForwardTraversal.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.Scanner;
+public class ForwardTraversal {
+    public static void arrayTraversal(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        arrayTraversal(arr);
+        sc.close();
+    }
+}
